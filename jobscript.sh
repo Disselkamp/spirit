99a8a19293de8b04e55c3eba715e106a48f76994
Constantin Disselkamp <constantin.disselkamp@rwth-aachen.de>
2018-03-17 17:21:35 +0100

    shell scripts for simulations on cluster

--- jobscript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -l
-module unload gcc
-module load gcc/6
-python ui-python/bobber_sktube.py 0.02 "bobber-paper-L12" 30 3 0.030 0.035 0.040 0.045 0.050 0.055 0.060 0.065 0.070 0.075 0.080 0.085 0.090 0.100
---
+++ scripts/test-direction.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -l
+python2.7 ui-python/bobber_sktube_2.py "test-direction_0.2" 30 -1 0.025 > ./test-direction.log 2>&1 &
