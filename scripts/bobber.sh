b2adf402e184a7c659d7fd6c0e8935dbf2c1e14e
Constantin Disselkamp <constantin.disselkamp@rwth-aachen.de>
2018-03-19 14:42:19 +0100

    simulation for same L

--- scripts/bobber.sh
+++ scripts/bobber.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-python2.7 ui-python/bobber_sktube.py "bobber-paper-L12_0.2" 30 -1 0.001 0.002 0.003 > ./bobber_1_20180317.log 2>&1 &
-python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.004 0.005 0.006 > ./bobber_2_20180317.log 2>&1 &
-python2.7 ui-python/bobber_sktube.py "bobber-paper-L16_0.2" 30 -1 0.007 0.008 > ./bobber_3_20180317.log 2>&1 &
-python2.7 ui-python/bobber_sktube.py "bobber-paper-L18_0.2" 30 -1 0.009 0.010 > ./bobber_4_20180317.log 2>&1 &
+python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.001 0.002 0.003 > ./bobber_1_20180319.log 2>&1 &
+python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.004 0.005 0.006 > ./bobber_2_20180319.log 2>&1 &
+python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.007 0.008 > ./bobber_3_20180319.log 2>&1 &
+python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.009 0.010 > ./bobber_4_20180319.log 2>&1 &
--- scripts/sktube.sh
+++ scripts/sktube.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-python2.7 ui-python/bobber_sktube.py "bobber-paper-L12_0.2" 30 -1 0.0001 0.0003 0.0005 > ./sktube_1_20180317.log 2>&1 &
-python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.0006 0.0008 0.0010 > ./sktube_2_20180317.log 2>&1 &
-python2.7 ui-python/bobber_sktube.py "bobber-paper-L16_0.2" 30 -1 0.003 0.005 0.007 > ./sktube_3_20180317.log 2>&1 &
-python2.7 ui-python/bobber_sktube.py "bobber-paper-L18_0.2" 30 -1 0.009 0.008 0.009 > ./sktube_4_20180317.log 2>&1 &
+python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.0001 0.0003 0.0005 > ./sktube_1_20180319.log 2>&1 &
+python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.0006 0.0008 0.0010 > ./sktube_2_20180319.log 2>&1 &
+python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.003 0.005 0.007 > ./sktube_3_20180319.log 2>&1 &
+python2.7 ui-python/bobber_sktube.py "bobber-paper-L14_0.2" 30 -1 0.009 0.008 0.009 > ./sktube_4_20180319.log 2>&1 &
